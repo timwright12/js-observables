34c40c821166e1575e5d3e704628a5dc3c4df8e5
Tim Wright <tim.wright@10up.com>
2018-10-16 14:04:09 -0400

    webpack clean up

--- demo-proxy/webpack.config.babel.js
+++ demo-proxy/webpack.config.babel.js
@@ -1,15 +1,12 @@
 import path from 'path';
 import webpack from 'webpack';
-import ExtractTextPlugin from 'mini-css-extract-plugin';
-import postcssPresetEnv from 'postcss-preset-env';
-import SpriteLoaderPlugin from 'svg-sprite-loader/plugin';
 
 const DIST_PATH = path.resolve( './dist' );
 
 const config = {
 	cache: true,
 	entry: {
-		proxy: './src/index.js',
+		index: './src/index.js',
 	},
 	output: {
 		path: DIST_PATH,
@@ -39,53 +36,11 @@ const config = {
 
 				}]
 			},
-			{
-				test: /\.css$/,
-				use: [
-					'style-loader',
-					{ loader: ExtractTextPlugin.loader },
-					{ loader: 'css-loader', options: { importLoaders: 1 } },
-					{ loader: 'postcss-loader', options: {
-						ident: 'postcss',
-						plugins: () => [
-							require('postcss-import')(),
-							postcssPresetEnv( {
-								stage: 0,
-								autoprefixer: {
-									grid: true
-								}
-							} ),
-							require('cssnano')()
-						]
-					} },
-					
-				]
-			},
-			{
-				test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
-				loader: 'url-loader',
-				options: {
-					limit: 10000
-				}
-			},
-			{
-				test: /assets\/src\/icons\/.*\.svg$/,
-				loader: 'svg-sprite-loader',
-				options: {
-					extract: true,
-					spriteFilename: './icons/svg-defs.svg',
-					runtimeCompat: true
-				}
-			},
 		]
 	},
 	mode: 'production',
 	plugins: [
 		new webpack.NoEmitOnErrorsPlugin(),
-		new ExtractTextPlugin( 'css/[name].css' ),
-		new SpriteLoaderPlugin({
-			plainSprite: true
-		})
 	],
 	stats: {
 		colors: true
--- demo-watch/webpack.config.babel.js
+++ demo-watch/webpack.config.babel.js
@@ -1,8 +1,5 @@
 import path from 'path';
 import webpack from 'webpack';
-import ExtractTextPlugin from 'mini-css-extract-plugin';
-import postcssPresetEnv from 'postcss-preset-env';
-import SpriteLoaderPlugin from 'svg-sprite-loader/plugin';
 
 const DIST_PATH = path.resolve( './dist' );
 
@@ -39,53 +36,11 @@ const config = {
 
 				}]
 			},
-			{
-				test: /\.css$/,
-				use: [
-					'style-loader',
-					{ loader: ExtractTextPlugin.loader },
-					{ loader: 'css-loader', options: { importLoaders: 1 } },
-					{ loader: 'postcss-loader', options: {
-						ident: 'postcss',
-						plugins: () => [
-							require('postcss-import')(),
-							postcssPresetEnv( {
-								stage: 0,
-								autoprefixer: {
-									grid: true
-								}
-							} ),
-							require('cssnano')()
-						]
-					} },
-					
-				]
-			},
-			{
-				test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
-				loader: 'url-loader',
-				options: {
-					limit: 10000
-				}
-			},
-			{
-				test: /assets\/src\/icons\/.*\.svg$/,
-				loader: 'svg-sprite-loader',
-				options: {
-					extract: true,
-					spriteFilename: './icons/svg-defs.svg',
-					runtimeCompat: true
-				}
-			},
 		]
 	},
 	mode: 'production',
 	plugins: [
 		new webpack.NoEmitOnErrorsPlugin(),
-		new ExtractTextPlugin( 'css/[name].css' ),
-		new SpriteLoaderPlugin({
-			plainSprite: true
-		})
 	],
 	stats: {
 		colors: true
